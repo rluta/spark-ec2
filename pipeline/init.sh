c1ae9250f18cbaca01e36c9fbbbad1fe3fbca89a
Raphael Luta <raphael.luta@aptiwan.com>
2016-01-08 14:44:19 +0100

    add pipeline module

---
+++ pipeline/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+pushd /root > /dev/null
+
+if [ -d "pipeline" ]; then
+  echo "Pipeline seems to be installed. Exiting."
+  return 0
+fi
+
+aws s3 cp s3://$PIPELINE_BUCKET/binaries/aldatalab-$PIPELINE_VERSION.tgz
+if [ $? != 0 ]; then
+  echo "ERROR: Unknown Pipeline version"
+  return -1
+fi
+
+echo "Unpacking Pipeline"
+tar xvzf aldatalab-*.tgz > /tmp/spark-ec2_pipeline.log
+rm aldatalab-*.tgz
+mv `ls -d aldatalab-*` pipeline
+
+popd > /dev/null
