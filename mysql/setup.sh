4342eaac5349ca9bce560997c377500bd135d46f
Raphael Luta <raphael.luta@aptiwan.com>
2016-08-16 13:10:29 +0200

    don't overwrite hive DB if already exists

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-mysqladmin create $HIVE_DB
-
-mysql $HIVE_DB <<EOF
+if mysqladmin create $HIVE_DB 2>&1 >/dev/null
+then
+    mysql $HIVE_DB <<EOF
 create user $HIVE_USER;
 grant usaage on *.* to `$HIVE_USER`@`localhost` identified by '$HIVE_PASSWORD';
 grant all to $HIVE_DB.* to `$HIVE_USER`@`localhost` identified by '$HIVE_PASSWORD';
 flush privileges;
 EOF
 
-cd /root/hive/sql
-mysql $HIVE_DB < hive-schema-1.2.0.mysql.sql
+    cd /root/hive/sql
+    mysql $HIVE_DB < hive-schema-1.2.0.mysql.sql
+else
+    echo "Mysql DB $HIVE_DB already exists, skipping setup"
+fi
