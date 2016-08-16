ae6b4a5a030d7e61a47fc410c8205e1885f8077b
Raphael Luta <raphael.luta@aptiwan.com>
2016-08-16 15:53:12 +0200

    ensure mysql is started before running setup scripts

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if mysqladmin create $HIVE_DB 2>&1 >/dev/null
+service mysqld start
+if mysqladmin create $HIVE_DB 2>/dev/null
 then
     mysql $HIVE_DB <<EOF
 create user $HIVE_USER;
-grant usaage on *.* to `$HIVE_USER`@`localhost` identified by '$HIVE_PASSWORD';
-grant all to $HIVE_DB.* to `$HIVE_USER`@`localhost` identified by '$HIVE_PASSWORD';
+grant usaage on *.* to $HIVE_USER@localhost identified by '$HIVE_PASSWORD';
+grant all to $HIVE_DB.* to $HIVE_USER@localhost identified by '$HIVE_PASSWORD';
 flush privileges;
 EOF
 
