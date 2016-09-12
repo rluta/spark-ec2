4a9c7fcac2395b67b279522400931e2d7ad634f6
Raphael Luta <raphael.luta@aptiwan.com>
2016-09-12 18:58:37 +0200

    * better formatting

--- zeppelin/configure-interpreters.sh
+++ zeppelin/configure-interpreters.sh
@@ -5,17 +5,20 @@ pushd /root/zeppelin >/dev/null
 # Update based on provided configs
 for interpreter in $*
 do
-    id=$(curl -s http://localhost:8080/api/interpreter/setting | jq -r ".body[]| select(.name == '$interpreter') |.id")
-    if [ ! -z "$id" ]; then
-        echo "Removing previous interpreter $interpreter ($id)"
-        curl -s -XDELETE http://localhost:8080/api/interpreter/setting/$sparkid
+    ids=$(curl -s http://localhost:8080/api/interpreter/setting | jq -r ".body[]| select(.name == \"$interpreter\") |.id")
+    if [ ! -z "$ids" ]; then
+        for id in $ids
+        do
+            echo "Removing previous interpreter $interpreter ($id)"
+            curl -s -XDELETE http://localhost:8080/api/interpreter/setting/$id  | jq '.'
+        done
     fi
 
     echo "Creating new $interpreter"
     int_file="conf/interpreters/${interpreter}.json"
     if [ -f "$int_file" ]
     then
-        curl -s -XPOST --data-binary @${int_file} http://localhost:8080/api/interpreter/setting
+        curl -s -XPOST --data-binary @${int_file} http://localhost:8080/api/interpreter/setting | jq '.'
     else
        echo "Warning: ${int_file} does not exist, ignoring $interpreter"
     fi
