5d70835cd1731c0e08a4614850591ede7ba36194
Raphael Luta <raphael.luta@aptiwan.com>
2016-10-27 11:45:20 +0200

    * Ensure elasticsearch user will be able to traverse /root directory

--- elasticsearch/setup.sh
+++ elasticsearch/setup.sh
@@ -9,6 +9,7 @@ SLAVES=`cat /root/spark-ec2/slaves`
 for slave in $SLAVES; do
     echo "Starting Elasticsearch $slave"
     ../spark-ec2/ssh-no-keychecking.sh $slave "useradd -r -U elasticsearch"
+    ../spark-ec2/ssh-no-keychecking.sh $slave "chmod 751 /root"
     ../spark-ec2/ssh-no-keychecking.sh $slave "chown -R elasticsearch /root/elasticsearch"
     ../spark-ec2/ssh-no-keychecking.sh -tt $slave "sudo -u elasticsearch -b /root/elasticsearch/bin/elasticsearch -d"
 done
